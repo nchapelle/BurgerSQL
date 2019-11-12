b37ff6dfe2284fdcb0f85770a645ec2e88cee26d
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2019-11-12 15:22:55 -0500

    we turned cats into burgers....general tso's may have been better

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name, devoured) VALUES ('Junior Bacon Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Shake Stack', true);
+INSERT INTO burgers (name, devoured) VALUES ('Burger Bomb', true);
+INSERT INTO burgers (name) VALUES ('Mushroom and Swiss');
